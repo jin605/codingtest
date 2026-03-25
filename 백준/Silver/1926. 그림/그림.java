a791a053a7cac89982a9d0c61b2abb78eee37e4e
jin605 <jinddd3@gmail.com>
2026-03-25 14:05:38 +0900

    [Silver I] Title: 그림, Time: 440 ms, Memory: 45308 KB -BaekjoonHub

--- 백준/Silver/1926. 그림/그림.java
+++ 백준/Silver/1926. 그림/그림.java
@@ -1,71 +1,91 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.StringTokenizer;
 
 public class Main {
-    static class Pair {
-        int x, y;
-        Pair(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
-    }
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        // n, m 입력 받기
-        if (!st.hasMoreTokens()) return;
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
 
         int[][] board = new int[n][m];
         boolean[][] vis = new boolean[n][m];
 
-        // 보드 데이터 입력
+        int[] dx = {1, -1, 0, 0};
+        int[] dy = {0, 0, 1, -1};
+
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < m; j++) {
+
                 board[i][j] = Integer.parseInt(st.nextToken());
             }
         }
-
-        int[] dx = {1, 0, -1, 0};
-        int[] dy = {0, 1, 0, -1};
 
         int count = 0;
         int maxArea = 0;
 
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (board[i][j] == 0 || vis[i][j]) continue;
+            for (int j = 0 ; j < m; j++) {
+
+                if (board[i][j] != 1 || vis[i][j])continue;
 
                 count++;
-                Queue<Pair> Q = new LinkedList<>();
                 vis[i][j] = true;
-                Q.add(new Pair(i, j));
+                Deque<Pair> que = new LinkedList<>();
+                que.offerLast(new Pair(i,j));
 
                 int curArea = 0;
-                while (!Q.isEmpty()) {
+                
+                
+                while (!que.isEmpty()) {
+                    
+                    Pair cur = que.pollFirst();
                     curArea++;
-                    Pair cur = Q.poll();
-
+                    
                     for (int dir = 0; dir < 4; dir++) {
+
                         int nx = cur.x + dx[dir];
                         int ny = cur.y + dy[dir];
-
-                        if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
-                        if (vis[nx][ny] || board[nx][ny] != 1) continue;
+                        
+                        if (nx < 0 || nx >=n || ny < 0 || ny >= m )continue;
+                        if (board[nx][ny] != 1 || vis[nx][ny])continue;
 
                         vis[nx][ny] = true;
-                        Q.add(new Pair(nx, ny));
+                        que.offerLast(new Pair(nx,ny));
+                        
                     }
                 }
-                if (curArea > maxArea) maxArea = curArea;
+                
+                if (curArea > maxArea) maxArea = curArea;  
+                
+                
+
             }
         }
 
         System.out.println(count);
         System.out.println(maxArea);
+
+
+
+
+    }
+
+    static class Pair {
+
+        int x, y;
+
+        public  Pair(int x,int y) {
+
+            this.x = x;
+            this.y = y;
+
+        }
     }
 }
