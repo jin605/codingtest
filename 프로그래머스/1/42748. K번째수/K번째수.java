c100e206cf7d0ef61abc1f9efa061972aca69fa7
jin605 <jinddd3@gmail.com>
2026-05-23 00:23:40 +0900

    [level 1] Title: K번째수, Time: 0.18 ms, Memory: 61.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] array, int[][] commands) {
+        
+        int[] answer = new int[commands.length];
+        
+        for (int i = 0; i < commands.length; i++) {
+            
+            int start = commands[i][0];
+            int end = commands[i][1]; 
+            int target = commands[i][2]; 
+            
+            ArrayList<Integer> list = new ArrayList<>();
+            
+            for (int j = start-1; j < end; j++) {
+                
+                list.add(array[j]);
+                
+            }
+            
+            list.sort(Comparator.naturalOrder());
+            answer[i] = list.get(target-1);
+                
+                
+        }
+        
+        
+        return answer;
+    }
+}
