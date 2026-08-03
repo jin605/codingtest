9f40f752724614598d7c13f597edd4d0f0bd0d17
jin605 <jinddd3@gmail.com>
2026-08-03 14:31:28 +0900

    [level 2] Title: 올바른 괄호, Time: 6.22 ms, Memory: 58.1 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -1,30 +1,25 @@
 import java.util.Deque;
 import java.util.ArrayDeque;
 
+
 class Solution {
     boolean solution(String s) {
-        boolean answer = false;
         
-        Deque <Character> queue = new ArrayDeque<>();
+        Deque <Character> stack = new ArrayDeque<>();
         
         for (int i = 0; i < s.length(); i++) {
-            
             char cur = s.charAt(i);
-            if (s.charAt(0) == ')') {
-                return false;
-            }
+            
             if (cur == '(') {
-                queue.offerFirst(cur);
-                
+                stack.offerFirst(cur);
             } else {
-                queue.pollFirst();
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                stack.pollFirst();
             }
         }
         
-        if (queue.isEmpty()) {
-            return true;
-        }
-
-        return answer;
+        return stack.isEmpty();
     }
 }
