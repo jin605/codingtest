bf17c0755d62b4b8df0e68abdb4e7991cdd6ee17
jin605 <jinddd3@gmail.com>
2026-08-05 23:29:28 +0900

    [level 2] Title: H-Index, Time: 3.54 ms, Memory: 83.2 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.java
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -1,34 +1,18 @@
-import java.util.*;
-    
+import java.util.Arrays;
+
 class Solution {
     public int solution(int[] citations) {
-        
-        int answer = 0;
-        ArrayList<Integer> list = new ArrayList<>();
-        
-        for (int i : citations) {
-            
-            list.add(i);
-        }
-        
-        list.sort(Comparator.reverseOrder());
-        
-        for (int i = 0; i < list.size(); i++) {
-            
-            int cur = list.get(i); // 현재 논문의 인용수
-            
-            if (cur >= i+1) {
-                
-                answer = i+1;
-                
-            } else {
-                return answer;
+        Arrays.sort(citations); 
+
+        int n = citations.length;
+
+        for (int i = 0; i < n; i++) {
+            int h = n - i;
+            if (citations[i] >= h) {
+                return h;
             }
-                
         }
-        
-        return answer;
-        
+
+        return 0;
     }
-        
 }
